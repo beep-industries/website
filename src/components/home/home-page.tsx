2d6d8725c7df771e19141c4e655a1edbd0151523
Dorian TETU <doriantetu34@gmail.com>
2026-02-25 15:40:55 +0100

    feat: adding a scroll on landing page

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -1,20 +1,31 @@
-import "@/styles/home-page.css"
-import Header from "@/components/home/header/header"
-import { HeroSection } from "@/components/home/hero-section/hero-section"
+"use client"
 import { Features } from "@/components/home/features/features"
-import { HomePageBackground } from "@/components/gradients/home-page-background"
-import { DocumentationSection } from "./documentation/documentation-section"
+import { HeroSection } from "@/components/home/hero-section/hero-section"
+import "@/styles/home-page.css"
+import { ChevronsDown } from "lucide-react"
 import { CollaborationSection } from "./collaborate/collaborate-section"
+import { DocumentationSection } from "./documentation/documentation-section"
 
 export function HomePage() {
   return (
-    <div className="relative flex min-h-screen h-full flex-col overflow-hidden">
-      <HomePageBackground />
+    <div className="relative flex h-full min-h-screen flex-col overflow-hidden">
       <main className="flex-grow">
-        <HeroSection />
-        <Features />
-        <DocumentationSection />
-        <CollaborationSection />
+        <div className="flex h-[calc(100dvh-64px)] flex-col items-center justify-between">
+          <div className="flex flex-col justify-center h-full pb-50">
+            <HeroSection />
+            <Features />
+          </div>
+          <button
+            className="mb-10 cursor-pointer text-white"
+            onClick={() => window.scrollTo({ top: window.innerHeight, behavior: "smooth" })}
+          >
+            <ChevronsDown className="h-6 w-6 animate-bounce" />
+          </button>
+        </div>
+        <div className="h-full">
+          <DocumentationSection />
+          <CollaborationSection />
+        </div>
       </main>
     </div>
   )
